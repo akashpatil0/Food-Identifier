58d01e6e1f5f01e77de7369efa96566e9c9408ed
Akash <akkusp@outlook.com>
2023-04-08 13:28:59 -0400

    temp

--- app/Food-Identifier/src/App.jsx
+++ app/Food-Identifier/src/App.jsx
@@ -1,22 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [image, setImage] = useState("");
 
   return (
     <div className="App">
       <h1>bitch give us an img pls</h1>
       <div className="upload-btn">
-       <input type = "file" accept='.jpeg, .jpg' onChange={(event) => {
-        const file = event.target.files[0];
-       }} />
+        <input
+          type="file"
+          accept=".jpeg, .jpg"
+          onChange={(event) => {
+            const file = event.target.files[0];
+          }}
+        />
       </div>
-     
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

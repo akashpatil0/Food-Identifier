ff4673f27613037c6004f2694628190f20349807
doristrungvuong@gmail.com <doristrungvuong@gmail.com>
2023-04-09 00:05:03 -0400

    nvjkfs

--- app/Food-Identifier/src/App.jsx
+++ app/Food-Identifier/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState, useRef, useCallback, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 import NutritionFacts from "./NutritionFacts";
 import Webcam from "react-webcam";
@@ -9,35 +8,37 @@ function App() {
   const [img, setImg] = useState(null);
   const webcamRef = useRef(null);
 
+  const getArray = (file) => {
+    const reader = new FileReader()
+
+    reader.onload = () => {
+      const byteArray = new Uint8Array(reader.result)
+      console.log(byteArray)
+    }
+    reader.readAsArrayBuffer(file) 
+  }
+
   const handleImageUpload = (event) => {
-    const file = event.target.files[0];
-    // go do sum tings with the img here
-  };
+    const file = event.target.files[0]
+    getArray(file)
+  } 
 
   const capture = useCallback(() => {
-    const imageSrc = webcamRef.current.getScreenshot();
-    setImg(imageSrc);
-  }, [webcamRef]);
+    const imageSrc = webcamRef.current.getScreenshot() 
+    setImg(imageSrc) 
+    console.log(imageSrc) 
+  }, [webcamRef]) 
 
   const videoConstraints = {
     width: 720,
     height: 480,
     imageSmoothing: true,
-  };
+  } 
 
   return (
     <div className="App">
       <h1>give us a pic</h1>
 
-      <div className="upload-btn">
-        <input
-          type="file"
-          accept=".jpeg, .jpg"
-          onChange={(event) => {
-            const file = event.target.files[0];
-          }}
-        />
-      </div>
       <input type="file" accept=".jpeg, .jpg" onChange={handleImageUpload} />
       <NutritionFacts />
       <div className="Container">
--- app/Food-Identifier/src/Camera.jsx
+++ app/Food-Identifier/src/Camera.jsx
@@ -14,7 +14,19 @@ const Camera = () => {
   const capture = useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();
     setImgSrc(imageSrc);
+    // get array
+    console.log("here1");
+    const reader = new FileReader();
+    reader.onload = () => {
+      const byteArray = new Uint8Array(reader.result);
+      console.log("here2");
+      console.log(byteArray);
+    };
+    reader.readAsArrayBuffer(fetch(imageSrc).then(res => res.blob()));
+
   }, [webcamRef, setImgSrc]);
+
+  
 
   return (
     <>

e8cc4bd8a662ae068e356ad595f53e382fdfc10f
Nathan Nussbaumer <nussbau65@gmail.com>
2023-04-13 18:32:25 -0400

    Made everything work better and look better

--- app/Food-Identifier/src/App.jsx
+++ app/Food-Identifier/src/App.jsx
@@ -1,43 +1,8 @@
-import { useState, useRef, useCallback, useEffect } from "react";
 import axios from "axios";
 
 import "./App.css";
-import NutritionFacts from "./NutritionFacts";
-import Webcam from "react-webcam";
 
 function App() {
-  const [img, setImg] = useState(null);
-  const [imgArray, setImgArray] = useState(null);
-  const webcamRef = useRef(null);
-
-  function readFile(file) {
-    return new Promise((resolve, reject) => {
-      // Create file reader
-      let reader = new FileReader()
-  
-      // Register event listeners
-      reader.addEventListener("loadend", e => resolve(e.target.result))
-      reader.addEventListener("error", reject)
-  
-      // Read file
-      reader.readAsArrayBuffer(file)
-    })
-  }
-
-  async function getAsByteArray(file) {
-    return new Uint8Array(await readFile(file))
-  }
-
-  const getArray = (file) => {
-    const reader = new FileReader()
-
-    reader.onload = () => {
-      const byteArray = new Uint8Array(reader.result)
-      byteArray
-      setImgArray(byteArray);
-    }
-    reader.readAsArrayBuffer(file) 
-  }
 
   const handleSubmit = (e) => {
     const file = e.target.files[0]
@@ -45,6 +10,7 @@ function App() {
 
     reader.onloadend = () => {
         var image = reader.result
+        document.body.style.backgroundImage = "url("+image+")"
         // Parameters
         const params = {image}
         axios
@@ -52,8 +18,7 @@ function App() {
         .then((res) => {
         const data = res.data.data
         const parameters = JSON.stringify(params)
-        const msg = `Prediction: ${data.prediction}`
-        alert(msg)
+        setNutritionLabel(data.prediction)
         })
         .catch((error) => alert(`Error: ${error.message}`)) 
     }  
@@ -61,55 +26,100 @@ function App() {
     reader.readAsDataURL(file)
   }
 
+  function getNutritionData(pred) {
+    const apiKey = "bYC8ueMGhttsf7ZuTSNtAaE0es5YmkFIrp4BiRTD"
+    const api_url = 'https://api.nal.usda.gov/fdc/v1/foods/search?api_key=' + apiKey + '&query=' + pred + '&dataType=Survey (FNDDS)'
+    return fetch(api_url).then(data => data.json())
+  }
 
+  function setNutritionLabel(pred) {
+    if (pred !== 'Not Recognized') {
+      getNutritionData(pred).then(data => {
+        console.log(data.foods)
+        const cal = data.foods[0].foodNutrients[3].value;
+        const fat = data.foods[0].foodNutrients[1].value;
+        const sat_fat = data.foods[0].foodNutrients[43].value;
+        const chol = data.foods[0].foodNutrients[42].value;
+        const carb = data.foods[0].foodNutrients[2].value;
+        const na = data.foods[0].foodNutrients[15].value;
+        const fiber = data.foods[0].foodNutrients[9].value;
+        const sugar = data.foods[0].foodNutrients[8].value;
+        const protein = data.foods[0].foodNutrients[0].value;
+        
+        document.getElementById("nut_title").innerHTML = ("Nutrition Label (" + data.foods[0].description + ")");
+        document.getElementById("cal").innerHTML = cal;
+        document.getElementById("fat").innerHTML = fat + 'g';
+        document.getElementById("sat_fat").innerHTML = sat_fat + 'g';
+        document.getElementById("chol").innerHTML = chol + 'mg';
+        document.getElementById("carb").innerHTML = carb + 'g';
+        document.getElementById("na").innerHTML = na + 'mg';
+        document.getElementById("fiber").innerHTML = fiber + 'g';
+        document.getElementById("sugar").innerHTML = sugar + 'g';
+        document.getElementById("protein").innerHTML = protein + 'g';
+      })
+    } else {
+      alert("Food not recognized")
+    }
+  }
 
-  const handleImageUpload = (event) => {
-    const file = event.target.files[0]
-    //handleSubmit(getArray(file))
-  } 
-
-  const capture = useCallback(() => {
-    const imageSrc = webcamRef.current.getScreenshot() 
-    setImg(imageSrc) 
-    const reader = new FileReader();
-  reader.onload = () => {
-    const byteArray = new Uint8Array(reader.result);
-    console.log(byteArray);
-  };
-  reader.readAsArrayBuffer(fetch(imageSrc).then(res => res.blob()));
-  }, [webcamRef]) 
-
-  const videoConstraints = {
-    width: 720,
-    height: 480,
-    imageSmoothing: true,
-  } 
 
   return (
     <div className="App">
-      <h1>give us a pic</h1>
-
-      <input type="file" accept=".jpeg, .jpg" onChange={handleSubmit} />
-      <NutritionFacts />
-      <div className="Container">
-        {img === null ? (
-          <>
-            <Webcam
-              ref={webcamRef}
-              screenshotFormat="image/jpeg"
-              videoConstraints={videoConstraints}
-            />
-            <button onClick={capture}>Capture Piture</button>
-          </>
-        ) : (
-          <>
-            <img src={img} alt="screenshot" />
-            <button onClick={() => setImg(null)}>Retake</button>
-          </>
-        )}
+      <div className="header">
+        <h1>Input a Picture</h1>
+        <div className="upload">
+          <input type="file" accept=".jpeg, .jpg" onChange={handleSubmit} />
+        </div>
+      </div>
+    
+      <div className="nutrition-label">
+        <h1 id="nut_title">Nutrition Facts</h1>
+        <table>
+          <tbody>
+            <tr>
+              <td>Calories</td>
+              <td id='cal'>95</td>
+            </tr>
+            <tr>
+              <td>Total Fat</td>
+              <td id='fat'>0.3g</td>
+            </tr>
+            <tr>
+              <td>Saturated Fat</td>
+              <td id='sat_fat'>0.1g</td>
+            </tr>
+            <tr>
+              <td>Cholesterol</td>
+              <td id='chol'>0mg</td>
+            </tr>
+            <tr>
+              <td>Sodium</td>
+              <td id='na'>1mg</td>
+            </tr>
+            <tr>
+              <td>Total Carbohydrate</td>
+              <td id='carb'>25g</td>
+            </tr>
+            <tr>
+              <td>Dietary Fiber</td>
+              <td id='fiber'>4g</td>
+            </tr>
+            <tr>
+              <td>Sugars</td>
+              <td id='sugar'>19g</td>
+            </tr>
+            <tr className="bottom-row">
+              <td>Protein</td>
+              <td id='protein'>0.5g</td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="footer">
+          * Percent Daily Values are based on a 2,000 calorie diet.
+        </div>
       </div>
     </div>
   );
 }
-
+   
 export default App;
